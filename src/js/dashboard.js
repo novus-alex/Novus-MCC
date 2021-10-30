b31f6242d45cad4d07e70bb48608433b9cfecd32
novus-alex <alexandrehachet1@gmail.com>
2021-10-30 13:38:09 +0200

    adding dashboard frame implementation

---
+++ src/js/dashboard.js
@@ -0,0 +1,44 @@
+$(document).ready(function() {
+	document.getElementById('content').src = "dashboard/overview.html";
+	dashboardHandler();
+	console.log('ok');
+})
+
+function dashboardHandler(e) {
+	switch (e.innerText.trim()) {
+		case ("Overview"):
+			removeActive();
+			e.classList.add("active");
+			document.getElementById('content').src = "dashboard/overview.html";
+			break;
+		case ("RF Settings"):
+			removeActive();
+			e.classList.add("active");
+			document.getElementById('content').src = "dashboard/rf_settings.html";
+			break;
+		case ("UART Settings"):
+			removeActive();
+			e.classList.add("active");
+			document.getElementById('content').src = "dashboard/uart_settings.html";
+			break;
+		case ("Chart Settings"):
+			removeActive();
+			e.classList.add("active");
+			document.getElementById('content').src = "dashboard/chart_settings.html";
+			break;
+		case ("Logs"):
+			removeActive();
+			e.classList.add("active");
+			document.getElementById('content').src = "dashboard/logs.html";
+			break;
+		default:
+			document.getElementById('content').src = "dashboard/overview.html";
+	}
+}
+
+function removeActive() {
+	var links = document.querySelectorAll('.nav-link');
+	for (var i = 0; i < links.length; i++) {
+		links[i].classList.remove('active')
+	}
+}
